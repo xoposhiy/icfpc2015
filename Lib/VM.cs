5dcb527e28f08160b6ddf26e727af6f901bd434c
Pavel Egorov <xoposhiy@mail.ru>
2015-07-30 15:23:59 +0500

    Initial solution with common dependencies

---
+++ Lib/VM.cs
@@ -0,0 +1,14 @@
+﻿using NLog;
+
+namespace Lib
+{
+    public class VM
+    {
+	    public static Logger log = LogManager.GetCurrentClassLogger();
+
+	    public VM()
+	    {
+			log.Info("Start");
+	    }
+    }
+}
