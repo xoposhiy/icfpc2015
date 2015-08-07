fd9c756a78054a5a7d87a1948c734fbbcfc57e84
unknown <pe@K1206022.kontur>
2015-08-07 20:41:55 +0500

    rotate point

--- Lib/Model/Model.cs
+++ Lib/Model/Model.cs
@@ -17,12 +17,12 @@ namespace Lib.Model
     }
 
 
-	class Model
-	{
-		public readonly int Height;
-		public readonly int Width;
-		public bool[,] Occupied { get; private set; }
-		public Figure Figure { get; private set; }
+    class Model
+    {
+        public readonly int Height;
+        public readonly int Width;
+        public bool[,] Occupied { get; private set; }
+        public Figure Figure { get; private set; }
 
         public bool IsSafeMovement(Directions direction)
         {
@@ -35,16 +35,92 @@ namespace Lib.Model
     {
         public int X { get; set; }
         public int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }        
+    }
+
+    struct PointF
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public PointF(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public PointF Rotate(PointF center, double angle)
+        {
+            var vector = new Vector(center, this).Rotate(angle);
+            return new PointF(vector.X + center.X, vector.Y + center.Y);
+        }
+    }
+
+    struct Vector
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        
+        public Vector(PointF center, PointF point)
+        {
+            X = point.X - center.X;
+            Y = point.Y - center.Y;
+        }
+
+        //CCW
+        public Vector Rotate(double angle)
+        {
+            return new Vector()
+            {
+                X = X * Math.Cos(angle) - Y * Math.Sin(angle),
+                Y = Y * Math.Cos(angle) + X * Math.Sin(angle)
+            };
+        }
     }
 
 
     class Figure
     {
-        public bool[,] Occupied;
+        public List<Point> Occupied;
         public Point PivotRelativeLocation;
         public bool IsSafePath(IEnumerable<Directions> path)
         {
             throw new NotImplementedException();
+        }
+
+        public void Move(Directions direction)
+        {
+            switch (direction)
+            {
+                case Directions.E:
+                    PivotRelativeLocation = new Point(PivotRelativeLocation.X + 1, PivotRelativeLocation.Y);
+                    return;
+
+                case Directions.W:
+                    PivotRelativeLocation = new Point(PivotRelativeLocation.X - 1, PivotRelativeLocation.Y);
+                    return;
+
+                case Directions.SE:
+                    PivotRelativeLocation = new Point(PivotRelativeLocation.X, PivotRelativeLocation.Y + 1);
+                    return;
+
+                case Directions.SW:
+                    PivotRelativeLocation = new Point(PivotRelativeLocation.X - 1, PivotRelativeLocation.Y + 1);
+                    return;
+
+                case Directions.CW:
+                                       
+                    return;
+                case Directions.CCW:
+                    return;
+            } 
+
         }
     }
 
---
+++ Lib/Tests/MoveTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+namespace Lib.Tests
+{
+    [TestFixture]
+    public class MoveTest
+    {
+        [TestCase()]
+        public void RotateCW(int pointX, int pointY, int centerX, int centerY)
+        {
+
+        }
+    }
+}
