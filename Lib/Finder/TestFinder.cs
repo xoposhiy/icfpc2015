6736b31c6c49fac9f913ac048bfd348a7a5dcf74
Kirill Borozdin <borozdin.kirill@gmail.com>
2015-08-07 17:59:21 +0500

    started writing unit test

---
+++ Lib/Finder/TestFinder.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Lib
+{
+    [TestFixture]
+    public class FinderTest
+    {
+        [Test]
+        public void Test()
+        {
+            var field = new bool[5, 10];
+            var figure = new FinderUnit(new PointInt[] { new PointInt(1, 0), new PointInt(2, 0) }, new PointInt(1, 0));
+            var target = new PointInt(1, 9);
+            var x = Finder.GetPath(field, figure, target);
+        }
+    }
+}
