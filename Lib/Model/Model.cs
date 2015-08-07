1304cb5561d8b1b0c1cc2e99f7eb05cea81b772e
okulovsky <yuri.okulovsky@gmail.com>
2015-08-07 18:54:09 +0500

    + model draft

---
+++ Lib/Model/Model.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.Model
+{
+    enum Directions
+    {
+        SE,
+        SW,
+        E,
+        W,
+        CW,
+        CCW
+    }
+
+
+	class Model
+	{
+		public readonly int Height;
+		public readonly int Width;
+		public bool[,] Occupied { get; private set; }
+		public Figure Figure { get; private set; }
+
+        public bool IsSafeMovement(Directions direction)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    struct Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+
+    class Figure
+    {
+        public bool[,] Occupied;
+        public Point PivotRelativeLocation;
+        public bool IsSafePath(IEnumerable<Directions> path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
