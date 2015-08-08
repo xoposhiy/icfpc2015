aa35d513d4373f98035b47576a943126c46426bc
okulovsky <yuri.okulovsky@gmail.com>
2015-08-08 12:25:04 +0500

    + form refactored, drawing is now in Grid class

--- ManualControl/Form.cs
+++ ManualControl/Form.cs
@@ -10,25 +10,39 @@ namespace ManualControl
     internal class TetrisForm : Form
     {
         private readonly Stack<Map> mapHistory = new Stack<Map>();
-        public int Radius = 20;
-        private readonly Dictionary<Keys, Directions> keymap;
-        private readonly int MapWidth;
-        private readonly int MapHeight;
-        private readonly int XMargin = 30;
-        private readonly int YMargin = 60;
-        private readonly Dictionary<Occupation, Pen> penTypes;
-        private readonly Dictionary<Occupation, Brush> brushTypes;
-        public Action<Directions> MovementRequested;
-
         public Map Map => mapHistory.Peek();
+        private readonly Dictionary<Keys, Directions> keymap;
+        public Action<Directions> MovementRequested;
+        Grid grid;
+        Label scores;
+        Label help;
 
-        public int LabelXOffset;
-        public int LabetYOffset;
         private bool showHelp;
 
         public TetrisForm(Map map)
         {
+            this.KeyPreview = true;
             this.mapHistory.Push(map);
+            grid = new Grid(mapHistory);
+            scores = new Label();
+            help = new Label();
+            help.Text = "UIOP - movement\r\nQW - rotate\r\nZ - undo\r\nL - lock";
+            help.BackColor = Color.Black;
+            help.Font = new Font("Arial", 10);
+            help.ForeColor = Color.Yellow;
+
+            Controls.Add(grid);
+            Controls.Add(scores);
+            Controls.Add(help);
+
+            scores.Size = new Size(100, 30);
+            grid.Location = new Point(0, 30);
+            grid.Size = grid.GetDesiredSize();
+            help.Size = new Size(150, 100);
+            help.Location = new Point(grid.Right, grid.Bottom - help.Height);
+            ClientSize = new Size(help.Right, help.Bottom);
+
+
             keymap = new Dictionary<Keys, Directions>
             {
                 [Keys.Q] = Directions.CCW,
@@ -38,26 +52,7 @@ namespace ManualControl
                 [Keys.O] = Directions.SE,
                 [Keys.P] = Directions.E
             };
-            MapWidth = map.Width;
-            MapHeight = map.Height;
-
-            penTypes = new Dictionary<Occupation, Pen>
-            {
-                [Occupation.Empty] = Pens.Black,
-                [Occupation.Occupied] = Pens.Red,
-                [Occupation.Unit] = Pens.LawnGreen,
-            };
-            brushTypes = new Dictionary<Occupation, Brush>
-            {
-                [Occupation.Empty] = Brushes.White,
-                [Occupation.Occupied] = Brushes.Gray,
-                [Occupation.Unit] = Brushes.LawnGreen,
-            };
-
-            ClientSize = new Size(
-                XMargin + (int)(Geometry.Width * Radius * (MapWidth + 1)),
-                YMargin + (int)(Geometry.YOffset * Radius * MapHeight + Geometry.Height * Radius));
-
+        
         }
 
         protected override void OnLoad(EventArgs e)
@@ -70,7 +65,7 @@ namespace ManualControl
 
         protected override void OnKeyDown(KeyEventArgs e)
         {
-            if (keymap.ContainsKey(e.KeyData) && MovementRequested != null)
+           if (keymap.ContainsKey(e.KeyData) && MovementRequested != null)
                 mapHistory.Push(Map.Move(keymap[e.KeyData]));
             if (e.KeyData == Keys.Z && mapHistory.Count > 1)
                 mapHistory.Pop();
@@ -81,75 +76,11 @@ namespace ManualControl
             if (e.KeyData == Keys.Escape)
                 showHelp = false;
             Invalidate();
+            grid.Invalidate();
+            scores.Text = Map.Scores.TotalScores.ToString();
         }
 
-        public void DrawHexagonInGraphicCoordinates(Graphics g, int gx, int gy, int kx, int ky, Pen pen, Brush brush, bool marked)
-        {
-            var w = (int)(Radius * Geometry.Width / 2);
-            var h = (int)(Radius * Geometry.Height / 2);
-
-            var points = new[]
-            {
-                new Point(gx + w, gy + Radius / 2),
-                new Point(gx + w, gy - Radius / 2),
-                new Point(gx, gy - h),
-                new Point(gx - w, gy - Radius / 2),
-                new Point(gx - w, gy + Radius / 2),
-                new Point(gx, gy + h),
-                new Point(gx + w, gy + Radius / 2)
-            };
-            g.FillPolygon(brush, points);
-            if (marked)
-            {
-                var f = 0.7f*w;
-                g.FillEllipse(Brushes.LightSkyBlue, gx -f, gy-f, 2*f, 2*f);
-            }
-            g.DrawLines(pen, points);
-            g.DrawString($"{kx - LabelXOffset},{ky - LabetYOffset}",
-                         new Font("Arial", Radius/2.5f),
-                         Brushes.Black,
-                         new Rectangle(gx - 100, gy - 100, 200, 200),
-                         new StringFormat { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center }
-                );
-        }
-
-        public void DrawHexagon(Graphics g, int x, int y, Pen pen, Brush brush, bool marked)
-        {
-            var p = Geometry.GetGeometricLocation(x, y);
-            DrawHexagonInGraphicCoordinates(g, (int)(Radius * p.X) + XMargin, (int)(Radius * p.Y) + YMargin, x, y, pen, brush, marked);
-        }
-
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            var g = e.Graphics;
-            g.Clear(Map.IsOver ? Color.LightCoral : Color.White);
-
-            g.DrawString(
-                Map.Scores.TotalScores.ToString(),
-                new Font("Arial", 15),
-                Brushes.Black,
-                new Point(0, 0)
-                );
-
-            for (var x = 0; x < MapWidth; x++)
-            {
-                for (var y = 0; y < MapHeight; y++)
-                {
-                    var p = new Point(x, y);
-                    var occupation = Map.Filled[x, y] ? Occupation.Occupied 
-                        : Map.Unit.Members.Any(m => m.Equals(p)) ? Occupation.Unit 
-                        : Occupation.Empty;
-                    DrawHexagon(g, x, y, penTypes[occupation], brushTypes[occupation], p.Equals(Map.Unit.PivotLocation));
-                }
-            }
-            if (showHelp)
-            {
-                g.FillRectangle(Brushes.Black, 0, 0, ClientSize.Width, 300);
-                g.DrawString("UIOP — movement", new Font("Arial", 10), Brushes.Yellow, 10, 10);
-                g.DrawString("QW — rotation", new Font("Arial", 10), Brushes.Yellow, 10, 30);
-                g.DrawString("Z — undo", new Font("Arial", 10), Brushes.Yellow, 10, 50);
-                g.DrawString("L — lock unit", new Font("Arial", 10), Brushes.Yellow, 10, 70);
-            }
-        }
+        
+       
     }
 }
