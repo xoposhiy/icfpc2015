d6178c40bc0b1736ab31093790095620e990a944
andrew <Andrew.Kostousov@gmail.com>
2015-08-08 13:26:03 +0500

    + submission scripts

---
+++ play_icfp2015/EntryPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace play_icfp2015
+{
+	class EntryPoint
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
