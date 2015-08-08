1bf20bedf6dfdc6574d55f45b63107d5f431baa7
asmirnova <asmirnova@skbkontur.ru>
2015-08-08 22:44:49 +0500

    MephalaOracle with different metrics

---
+++ Lib/Intelligence/MephalaOracle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lib.Finder;
+using Lib.Models;
+
+namespace Lib.Intelligence
+{
+    public class MephalaOracle: IOracle
+    {
+        private readonly Func<Map, PositionedUnit, double> metrics;
+        private readonly IFinder finder;
+
+        public MephalaOracle(IFinder finder, Func<Map, PositionedUnit, double> metrics)
+        {
+            this.metrics = metrics;
+            this.finder = finder;
+        }
+         
+        public IEnumerable<OracleSuggestion> GetSuggestions(Map map)
+        {
+            var positions = finder.GetReachablePositions(map);
+
+            var suggestions = new List<OracleSuggestion>();
+            foreach (var position in positions)
+            {
+                var positionedUnit = map.Unit.WithNewPosition(position);
+
+                foreach (var dir in OracleServices.GetAllDirections())
+                {
+                    var nextPosition = positionedUnit.Move(dir);
+                    if (!map.IsValidPosition(nextPosition))
+                    {
+                        suggestions.Add(new OracleSuggestion(position, dir, metrics(map, positionedUnit)));
+                        break;
+                    }
+                }
+            }
+            return suggestions.OrderByDescending(s => s.Metrics);
+        }
+    }
+}
